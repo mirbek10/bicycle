a130d2799a9dd0fbd99eb4ac849cbb3920f532ac
Nurislam <nurixkg5@gmail.com>
2025-03-27 01:31:04 +0800

    added header and adaptive

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,44 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import './header.scss'
-import { useDispatch } from 'react-redux';
-
-
+import React from 'react';
+import logo from "../../assets/svg/logo.svg";
+import heart from "../../assets/svg/heart.svg";
+import cart from "../../assets/svg/cart.svg";
+import search from "../../assets/svg/search.svg";
+import profile from "../../assets/svg/profile.svg";
+import menu from "../../assets/svg/menu.svg";
+import { Link } from 'react-router-dom';
+import './header.scss';
 
 function Header() {
-    const dispatch = useDispatch();
-
     return (
         <header className='header'>
-            header
+            <div className="header-content container">
+                <div className="header-left">
+                    <img src={logo} alt="Logo" />
+                </div>
+                <div className="header-right">
+                    <div className="main-menu">
+                        <ul>
+                            <Link className='main-li'><li>Trade In</li></Link>
+                            <Link className='main-li'><li>Велосипеды</li></Link>
+                            <Link className='main-li'><li>Запчасти</li></Link>
+                            <Link className='main-li'><li>Экипировка</li></Link>
+                            <Link className='main-li'><li>Аксессуары</li></Link>
+                            <Link className='main-li'><li>Велостанки</li></Link>
+                        </ul>
+                    </div>
+                    <div className="logo-menu">
+                        <img src={search} alt="Search" />
+                        <img src={profile} alt="Profile" />
+                        <img src={heart} alt="Favorites" />
+                        <img src={cart} alt="Cart" />
+                    </div>
+                    <div className="header-menu">
+                        <img src={menu} alt="Menu" />
+                    </div>
+                </div>
+            </div>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
