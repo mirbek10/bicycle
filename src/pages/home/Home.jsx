69b626a7acbd2a1a0d41f380f3da73166ed7c3c8
Nurislam <nurixkg5@gmail.com>
2025-03-29 17:05:43 +0800

    added mountain

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../../Components/banner/Banner'
 import EuroBrand from '../../Components/euro-brand/EuroBrand'
+import Mountain from '../../Components/mountain/Mountain'
 
 function Home() {
     return (
         <div>
             <Banner/>
             <EuroBrand/>
+            <Mountain/>
         </div>
     )
 }
