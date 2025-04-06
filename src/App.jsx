73801642b350136a4d3a7096efe64eb16c682bbb
Nurislam <nurixkg5@gmail.com>
2025-04-07 03:33:00 +0800

    added changePaswword

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
-import './App.css'
-import { RouterProvider } from 'react-router-dom'
-import { myRouter } from './routes/router'
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { myRouter } from './routes/router';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
+import { setAuth } from './store/authSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true); 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(setAuth({
+          isAuthenticated: true,
+          user: {
+            uid: user.uid,
+            email: user.email,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+        }));
+      } else {
+        dispatch(setAuth({
+          isAuthenticated: false,
+          user: null,
+        }));
+      }
+      setLoading(false); 
+    });
+
+    return () => unsubscribe();
+  }, [dispatch]);
+
+  if (loading) {
+    return <div className="loading">Загрузка...</div>; 
+  }
 
   return (
     <div className='wrapper'>
-     <RouterProvider router={myRouter} />
+      <RouterProvider router={myRouter} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
 
 
 
