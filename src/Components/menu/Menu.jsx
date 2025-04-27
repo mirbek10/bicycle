b129324708a772bb0e4fcc6d93202b8d037e1d4b
aidana <ulanovaaidana2@gmail.com>
2025-04-27 09:24:39 +0600

    added menu styles

--- src/Components/menu/Menu.jsx
+++ src/Components/menu/Menu.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Menu.module.scss';
+
 import img1 from '../../assets/image/MirbekImg/menu1img.png';
 import img2 from '../../assets/image/MirbekImg/menu2img.png';
 import img3 from '../../assets/image/MirbekImg/menu3img.png';
@@ -7,51 +9,69 @@ import img4 from '../../assets/image/MirbekImg/menu4img.png';
 import img5 from '../../assets/image/MirbekImg/menu5img.png';
 import img6 from '../../assets/image/MirbekImg/menu6img.png';
 
-
-
 const Menu = () => {
+  return (
+    <div className={styles.menu}>
+      <h1>КАТАЛОГ</h1>
+      <ul>
+        {/* Велосипеды */}
+        <Link to="/category/Велосипеды" className={styles.link}>
+          <div className={`${styles.bike} ${styles.black}`}>
+            <li>Велосипеды</li>
+            <img src={img1} alt="Велосипеды" />
+          </div>
+        </Link>
 
+        <div className={styles.downblock}>
+          {/* Trade-in */}
+          <Link to="/category/Trade-in" className={styles.link}>
+            <div className={`${styles.tradein} ${styles.black}`}>
+              <li>Trade-in</li>
+              <img src={img2} alt="Trade-in" />
+            </div>
+          </Link>
 
-    return (
-        <div className={styles.menu}>
-            <h1>КАТАЛОГ</h1>
-            <ul>
-                <div className={`${styles.bike} ${styles.black}`}>
-                    <li>Велосипеды</li>
-                    <img src={img1} alt="" />
+          <div className={styles.downleftblock}>
+            <div className={styles.miniblock1}>
+              {/* Запчасти */}
+              <Link to="/category/Запчасти" className={styles.link}>
+                <div className={`${styles.hellper} ${styles.black}`}>
+                  <li>Запчасти</li>
+                  <img src={img3} alt="Запчасти" />
                 </div>
-                <div className={styles.downblock}>
-                    <div className={`${styles.tradein} ${styles.black}`}>
-                        <li>Trade-in</li>
-                        <img src={img2} alt="" />
-                    </div>
-                    <div className={styles.downleftblock}>
+              </Link>
 
-                        <div className={styles.miniblock1}>
-                            <div className={`${styles.hellper} ${styles.black}`}>
-                                <li>Запчасти</li>
-                                <img src={img3} alt="" />
-                            </div>
-                            <div className={`${styles.аксесуар} ${styles.black}`}>
-                                <img src={img4} alt="" />
-                                <li>Аксессуары</li>
-                            </div>
-                        </div>
-                        <div className={styles.miniblock2}>
-                            <div className={`${styles.экипировка} ${styles.black}`}>
-                                <li>Экипировка</li>
-                                <img src={img5} alt="" />
-                            </div>
-                            <div className={`${styles.велостанки} ${styles.black}`}>
-                                <li>Велостанки</li>
-                                <img src={img6} alt="" />
-                            </div>
-                        </div>
-                    </div>
+              {/* Аксессуары */}
+              <Link to="/category/Аксессуары" className={styles.link}>
+                <div className={`${styles.аксесуар} ${styles.black}`}>
+                  <li>Аксессуары</li>
+                  <img src={img4} alt="Аксессуары" />
                 </div>
-            </ul>
+              </Link>
+            </div>
+
+            <div className={styles.miniblock2}>
+              {/* Экипировка */}
+              <Link to="/category/Экипировка" className={styles.link}>
+                <div className={`${styles.экипировка} ${styles.black}`}>
+                  <li>Экипировка</li>
+                  <img src={img5} alt="Экипировка" />
+                </div>
+              </Link>
+
+              {/* Велостанки */}
+              <Link to="/category/Велостанки" className={styles.link}>
+                <div className={`${styles.велостанки} ${styles.black}`}>
+                  <li>Велостанки</li>
+                  <img src={img6} alt="Велостанки" />
+                </div>
+              </Link>
+            </div>
+          </div>
         </div>
-    );
+      </ul>
+    </div>
+  );
 };
 
 export default Menu;
